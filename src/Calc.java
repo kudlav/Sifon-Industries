ef9d825cd60db8c6ba9ed6b4f9e8489eaa374d9d
kudlav <vladankudlac@gmail.com>
2017-04-04 23:53:00 +0200

    Added main class Calc, fixed warnings of GUI
    fixed fx-font-size at styles.css
    removed package ivs2 from FXMLDocumentController

---
+++ src/Calc.java
@@ -0,0 +1,30 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Vladan
+ */
+public class Calc extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+
+		Scene scene = new Scene(root);
+
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
--- src/FXMLDocumentController.java
+++ src/FXMLDocumentController.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ivs2;
 
 import java.net.URL;
 import java.util.ResourceBundle;
